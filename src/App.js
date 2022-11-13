2d02f4ad7e75241b83c37c3b5e57b64ddd4af011
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-13 19:24:38 +0600

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='container mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
